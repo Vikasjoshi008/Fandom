258e8306fbf1523778a58f08373787e9aaa85fb1
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-05-07 21:53:08 +0530

    feat: integrated cookie-based auth for login and register

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
 
-const App = () => {
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+        <Route
+          path="*"
+          element={<div className="text-center mt-20">Page Not Found</div>}
+        />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
